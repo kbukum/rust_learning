e2a5647d05951ebb0b1ccf9d24125185128970e8
Kamil BUKUM <kamilbukum@gmail.com>
2022-04-22 01:47:27 -0400

    - Removed generated docs.

--- doc/search-index.js
+++
@@ -1,4 +0,0 @@
-var searchIndex = JSON.parse('{\
-"english":{"doc":"This module contains English phareses.","t":[5,5],"n":["goodbye","hello"],"q":["english",""],"d":["",""],"i":[0,0],"f":[[[],["string",3]],[[],["string",3]]],"p":[]}\
-}');
-if (window.initSearch) {window.initSearch(searchIndex)};
--- doc/source-files.js
+++
@@ -1,3 +0,0 @@
-var N = null;var sourcesIndex = {};
-sourcesIndex["english"] = {"name":"","files":["english.rs"]};
-createSourceSidebar();
